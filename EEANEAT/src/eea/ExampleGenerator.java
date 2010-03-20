66a0c2b4e1d7076ac495b157c8786e4e43e325bc
Eshwaran.VijayKumar <Eshwaran.VijayKumar@d105a0b8-b78d-11de-92a3-373a1addbe3b>
2010-03-20 17:42:50 +0000

    Reorganization continues

--- EEANEAT/src/eea/ExampleGenerator.java
+++
@@ -1,5 +0,0 @@
-package eea;
-
-import java.util.List;
-
-public interface ExampleGenerator<T>{List<T> generateExample();}
--- EEANEAT/src/eea/Pair.java
+++
@@ -1,5 +0,0 @@
-package eea;
-
-public class Pair<T, E> {
-	public T first;public E second;public Pair(T f, E s){first = f; second = s;}
-}
